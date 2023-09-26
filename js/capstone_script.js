212b8e46466c29ec40e3b8684efbbfb7f12702b7
unknown <64100803+d-ramirez88@users.noreply.github.com>
2023-09-25 19:40:18 -0600

    js curse

--- js/capstone_script.js
+++ js/capstone_script.js
@@ -1,9 +1,8 @@
-function getMyDate(){
-	var today = Date();
-	var death = new Date(3/15/1937);
-	var diffTime = Math.abs(today-death);
-	var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-	console.log(death);
-	
-	document.getElementById("currentDate").innerHTML=diffDays;
+function setCurse(state){
+	if (state) {
+		document.getElementById("mainBody").classList.add("cursed");
+	}
+	else {
+		document.getElementById("mainBody").classList.remove("cursed");
+	}
 }
