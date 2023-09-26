b8460ef8d913247bde1dbb5475af6b1178daf131
unknown <64100803+d-ramirez88@users.noreply.github.com>
2023-09-25 20:00:08 -0600

    fonts

--- js/capstone_script.js
+++ js/capstone_script.js
@@ -1,8 +1,10 @@
 function setCurse(state){
 	if (state) {
 		document.getElementById("mainBody").classList.add("cursed");
+		document.getElementById("trapped").style.display="inline"
 	}
 	else {
 		document.getElementById("mainBody").classList.remove("cursed");
+		document.getElementById("trapped").style.display="none";
 	}
 }
